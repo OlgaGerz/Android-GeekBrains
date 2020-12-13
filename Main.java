e398a11fbe6f95422f6cedd31de5d250a7f8b4db
OlgaGerz <twerskaya3@mail.ru>
2020-12-13 12:04:16 +0300

    Урок 5. Домашнее задание.

--- Main.java
+++ Main.java
@@ -1,49 +1,28 @@
-package ru.geekbrains;
-import java.util.Scanner;
+package Lesson5HomeWork;
+
+import lesson5.Cat;
 
 public class Main {
+public static void main (String[] args) {
 
-    public static void main(String[] args) {
-
-        int a = 0;
-        int b = 9;
-        int randomNumber = a + (int) (Math.random() * b);
-
-
-        System.out.println("Угадайте число от 0 до 9 ");
+    Employee employee1 = new Employee("Иван Иванович Иванов","Начальник", "ivanov@mail.ru", "+75674563423", 123566, 67);
+    Employee employee2 = new Employee("Вероника Владимировна Сидорова", "Заместитель начальника", "sidorova@mail.ru", "+74561238945", 100456, 50);
+    Employee employee3 = new Employee("Дмитрий Сергеевич Петров", "Главный специалист", "petrov@mail.ru", "+73452345678", 98567, 42);
+    Employee employee4 = new Employee("Мария Ивановна Данилова", "Ведущий специалист", "danilova@mail.ru", "+78964534567", 90567, 35);
+    Employee employee5 = new Employee("Сергей Олегович Костин", "Специалист", "kostin@mail.ru", "+78456335678", 50000, 25);
 
 
-        int userCounter = 3;
-        int userNumber = getUserNumber();
-        while (userCounter != 0) {
-            System.out.println("Угадайте число от 0 до 9 ");
-        }
-        for (int i = 3; i < 3; i--) {
-            System.out.println("У вас осталось попыток: " + i);
-            if (randomNumber == userNumber) {
-                System.out.println("Вы угадали!/n Повторить игру еще раз? 1 - да / 0 - нет");
-                break;
-            } else if (randomNumber < userNumber) {
-                System.out.println("Ваше число больше, чем нужно. У вас осталось попыток: " + userCounter);
-                userCounter--;
-                continue;
-            } else if (randomNumber > userNumber) {
-                System.out.println("Ваше число меньше, чем нужно. У вас осталось попыток: " + userCounter);
-                userCounter--;
-                continue;
-            }
+    Employee[] employees = {employee1, employee2, employee3, employee4, employee5};
 
-        }
+
+
+for (Employee employee : employees) {
+    if (employee.getAge() > 40) {
+        employee.printInfo();
     }
 
-    private static int getUserNumber() {
-        Scanner number = new Scanner(System.in);
-        int userNumber = number.nextInt();
-        return userNumber;
-    }
 }
 
 
-
-
-
+}
+}
